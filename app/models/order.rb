06fd1861327cec1b50557d9b7e5254d2cbbaa9cc
Cássio Maletich <cgm1010@gmail.com>
2021-02-06 19:59:40 -0300

    avoid 0 value @ order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user, inverse_of: :orders
 
-  validates_presence_of :value, :imei, :device
+  validates_presence_of :imei, :device
+  validates :value, presence: true, numericality: { greater_than: 0 }
 end
