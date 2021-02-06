a5ee2d232dc32b5f51119ced3cb53fe8786f9211
Cássio Maletich <cgm1010@gmail.com>
2021-02-06 15:35:58 -0300

    user validation & model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :orders
+
+    validates_presence_of :name, :cpf
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user" do
+    user = User.new(name: "Cássio", email: "cgm1010@gmail.com", cpf: "00000000000")
+    assert user.valid?
+  end
+
+  test "invalid user - missing name" do
+    user = User.new(email: "cgm1010@gmail.com", cpf: "00000000000")
+    assert_not user.valid?
+    assert_not_empty user.errors["name"]
+  end
+
+  test "invalid user - missing cpf" do
+    user = User.new(name: "Cássio", email: "cgm1010@gmail.com")
+    assert_not user.valid?
+    assert_not_empty user.errors["cpf"]
+  end
+
+  test "invalid user - missing email" do
+    user = User.new(name: "Cássio", cpf: "00000000000")
+    assert_not user.valid?
+    assert_not_empty user.errors["email"]
+  end
+
+  test "invalid user - invalid email" do
+    user = User.new(name: "Cássio", email: "cgm1010@gmail", cpf: "00000000000")
+    assert_not user.valid?
+    assert_not_empty user.errors["email"]
+  end
 end
