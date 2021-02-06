ed8fbe11c5142143ae698b53daac20ce611a6511
Cássio Maletich <cgm1010@gmail.com>
2021-02-06 15:52:44 -0300

    more user validations & controller test fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :orders
+    has_many :orders, dependent: :destroy
 
-    validates_presence_of :name, :cpf
+    validates_presence_of :name
+    validates :cpf, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { cpf: @user.cpf, email: @user.email, name: @user.name } }
+      post users_url, params: { user: { cpf: "00011100066", email: "test@mail.com", name: @user.name } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { cpf: @user.cpf, email: @user.email, name: @user.name } }
+    patch user_url(@user), params: { user: { cpf: "00001111", email: "user@mail.com", name: @user.name } }
     assert_redirected_to user_url(@user)
   end
 
