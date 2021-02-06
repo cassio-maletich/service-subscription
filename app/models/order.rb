c010539cba29595dd4b21146b3150340ddb04d31
Cássio Maletich <cgm1010@gmail.com>
2021-02-06 15:18:30 -0300

    validating order fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :value, :imei, :device
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "valid order" do
+    order = Order.new(value: 100, imei: "798789789798", device: "Redmi Note 8", user: @user)
+    assert order.valid?
+  end
+
+  test "invalid order - missing value" do
+    order = Order.new(imei: "798789789798", device: "Redmi Note 8", user: @user)
+    assert_not order.valid?
+    assert_not_empty order.errors["value"]
+  end
+
+  test "invalid order - missing imei" do
+    order = Order.new(value: 100, device: "Redmi Note 8", user: @user)
+    assert_not order.valid?
+    assert_not_empty order.errors["imei"]
+  end
+
+  test "invalid order - missing device" do
+    order = Order.new(value: 100, imei: "798789789798", user: @user)
+    assert_not order.valid?
+    assert_not_empty order.errors["device"]
+  end
+
+  test "invalid order - missing user" do
+    order = Order.new(value: 100, imei: "798789789798", device: "Redmi Note 8")
+    assert_not order.valid?
+    assert_not_empty order.errors["user"]
+  end
 end
